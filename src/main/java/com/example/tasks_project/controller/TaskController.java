3ccc941d0551641a0d89dbec8126471e10213ea7
helldiving <142443321+helldiving@users.noreply.github.com>
2024-09-30 15:44:18 -0400

    Initial outline

--- src/main/java/com/example/tasks_project/controller/TaskController.java
+++ src/main/java/com/example/tasks_project/controller/TaskController.java
@@ -1,4 +1,13 @@
 package com.example.tasks_project.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TaskController {
+    @GetMapping("/tasks")
+    public String getAllTasks() {
+        return "Returns all tasks";
+    }
 }
--- src/main/java/com/example/tasks_project/model/Task.java
+++ src/main/java/com/example/tasks_project/model/Task.java
@@ -1,4 +1,13 @@
 package com.example.tasks_project.model;
 
+import java.util.Date;
+
 public class Task {
+    private String title;
+    private String description;
+    private boolean completed;
+    private Date createDate;
+    private Date completedDate; // email said 'completed' but changed to 'completedDate'
+
 }
+
